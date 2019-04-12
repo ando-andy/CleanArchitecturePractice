283ed3c334c4fa65ac18abadc9d73ecc27ab1a9f
ando-andy <ak.yk.kt.free.space@gmail.com>
2019-04-12 15:38:19 +0700

    Initial Commit

---
+++ CleanArchitecturePractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanArchitecturePractice
+//
+//  Created by 安藤 on 2019/04/12.
+//  Copyright © 2019 Kazando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
