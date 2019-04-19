e405d204bb37d2aee79f4582089dc2d5c1acf605
ando-andy <36369031+ando-andy@users.noreply.github.com>
2019-04-19 18:56:45 +0700

    Update Assembler.swift

--- CleanArchitecturePractice/Assembler/Assembler.swift
+++ CleanArchitecturePractice/Assembler/Assembler.swift
@@ -1,11 +1,3 @@
-//
-//  Assembler.swift
-//  CleanArchitecture
-//
-//  Created by Tuan Truong on 7/15/18.
-//  Copyright © 2018 Framgia. All rights reserved.
-//
-
 protocol Assembler: class,
     MainAssembler,
     LoginAssembler,
